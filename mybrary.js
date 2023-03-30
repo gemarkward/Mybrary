cbdd7b0f1d9c7d448d9c71301dd3e6eda1028991
Gilbert Markward <gilbert.markward@ymail.com>
2023-03-30 15:00:07 -0400

    Fix add and delete buttons

--- mybrary.js
+++ mybrary.js
@@ -1,11 +1,20 @@
-let myLibrary = [];
+let myLibrary = {};
+let rowID = 0;
 
 function updateLibraryDisplay(element) {
   let table = $("#libraryDisplay");
   let popUp = $("#submitBook");
-  var newRow = "'<tr><td>"+element.title+"</td><td>"+element.author+"</td><td>"+element.pages.toString()+"</td><td>"+element.read.toString()+"</td></tr>'";
-  console.log(newRow);
+  var rowStr = parseInt(element.rowID).toString();
+  var newRow = "'<tr id=\""+rowStr+"\"><td>"+element.title+"</td><td>"+element.author+"</td><td>"+element.pages.toString()+"</td><td>"+element.read.toString()+"</td><td><button class=\"deleteBook\">Delete Book</button></tr>'";
   table.append(newRow);
+
+  $( ".deleteBook ").on( "click", function(element) {
+    var rowStr = parseInt(element.rowID).toString();
+    var rowToDelete = $("#"+rowStr);
+    rowToDelete.remove();
+    delete myLibrary[element.rowID];
+  });
+
   popUp.css("display", "none");
 }
 
@@ -15,13 +24,15 @@ class Book {
     this.author = author;
     this.pages = nPages;
     this.read = read;
+    this.id = rowID;
+    rowID += 1;
   }
 
   addToLibrary() {
-    myLibrary.push(this);
+    myLibrary[rowID] = this;
     updateLibraryDisplay(this);
   }
-}
+};
 
 $(() => {
 
